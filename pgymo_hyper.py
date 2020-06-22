0df5d2ee38c431b3df418f902dd7ac0e7606d7ca
Joseph Kasprzyk <joseph.kasprzyk@colorado.edu>
2020-06-22 14:05:03 -0600

    Finalized Hypervolume Example

--- pgymo_hyper.py
+++ pgymo_hyper.py
@@ -1,23 +1,36 @@
-from pygmo import *
+import pygmo
 import timeit
+import numpy as np
 
-# https://stackoverflow.com/questions/6583573/how-to-read-numbers-from-file-in-python
-# This opens a handle to your file, in 'r' read mode
-file_handle = open('reference_set.txt', 'r')
-# Read in all the lines of your file into a list of lines
-lines_list = file_handle.readlines()
-my_data = [[float(val) for val in line.split()] for line in lines_list]
+# read numbers from a file using numpy array
+my_data = np.genfromtxt('reference_set.txt', delimiter=" ")
 
-ref_point = [0.0]*9
+# the above has the decisions and objectives
+# for example:
+# 14 decisions, therefore 0:ndec are decisions
+# 9 objectives, therefore ndec:ndec+nobj are objectives
+ndec = 14
+nobj = 9
+nconstr = 9
 
-print("5 rows")
-hv_from_list = hypervolume(my_data[0:5][:])
-start_time = timeit.default_timer()
-hv_from_list.compute(ref_point)
-print(timeit.default_timer - start_time)
+# to find the reference point we use the pygmo2 guidance:
+#We assume minimization in every dimension, that is, a reference point is required to be numerically larger or equal in each objective, and strictly larger in at least one of them.
 
+ref_point = [0.0]*nobj
+ref_point_scaling = 1.5
 
-# # Extract dimensions from first line. Cast values to integers from strings.
-# cols, rows = (float(val) for val in lines_list[0].split())
-# # Do a double-nested list comprehension to get the rest of the data into your matrix
-# my_data = [[float(val) for val in line.split()] for line in lines_list[1:]]
+j = 0
+for i in range(ndec, ndec+nobj):
+    ref_point[j] = ref_point_scaling*max(my_data[:,i])
+    if ref_point[j] < 0.0:
+        ref_point[j] = 0.0
+    print (f'objective col = {i}, ref = {ref_point[j]}')
+    j = j+1
+
+row_tests = [30, 50, 100, 200, 300, 507]
+
+for row in row_tests:
+    start_time = timeit.default_timer()
+    hv_from_list = pygmo.hypervolume(my_data[0:row, ndec:ndec+nobj])
+    my_hv_result = hv_from_list.compute(ref_point)
+    print(f'For {row} rows, the hv is {my_hv_result} and the time is {timeit.default_timer() - start_time}.')
